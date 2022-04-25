f31f65fe58611b4cd14942017f8bc714be04594f
My.Project <khuong.kyuubi@gmail.com>
2022-04-25 13:39:46 +0700

    Thêm bài thực hành sử dụng spread syntax

---
+++ thuc-hanh-su-dung-spread-attributes-trong-es-6/js/main.js
@@ -0,0 +1,11 @@
+const cars1 = ["Audi", "BMW", "Lexus", "Mercedes"];
+// copy form cars1 to cars2 using spread
+const cars2 = [...cars1];
+
+// copy prom cars1 to cars3 , add more elements
+//
+const cars3 = [...cars1,"Peugeot", "Roll Royce" ]
+
+console.log(cars1);
+console.log(cars2);
+console.log(cars3);
